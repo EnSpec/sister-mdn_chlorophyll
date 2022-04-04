d9bb120f0b62aa09d7edaa8dc50af7ed357b1a7b
adamchlus <23018310+adamchlus@users.noreply.github.com>
2022-04-04 09:26:42 -0500

    Added imgspec bash script

---
+++ .imgspec/get_paths_from_granules.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+
+def main():
+
+    # Unzip and untar granules
+    input_dir = "input"
+
+    dirs = [d for d in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, d))]
+    instrument = "PRISMA" if dirs[0][:3] == "PRS" else "AVIRIS"
+
+    # Get paths based on product type file matching
+    paths = []
+
+    if instrument == "PRISMA":
+        paths+= glob.glob(os.path.join(input_dir, "*", "*rfl_prj"))
+
+    elif instrument == "AVIRIS":
+        paths+= glob.glob(os.path.join(input_dir, "*rfl*", "*rfl")
+        paths+= glob.glob(os.path.join(input_dir, "*rfl*", "*corr*img"))
+
+    for path in paths:
+        print(path)
+
+if __name__ == "__main__":
+    main()
---
+++ .imgspec/imgspec_run.sh
@@ -0,0 +1,22 @@
+imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
+pge_dir=$(dirname ${imgspec_dir})
+
+mkdir output
+tar_file=$(ls input/*tar.gz)
+#echo $tar_file
+base=$(basename $tar_file)
+#echo $base
+output_dir=output/${base%.tar.gz}
+#echo $output_dir
+mkdir $output_dir
+
+tar -xzvf $tar_file -C input
+
+for a in `python get_paths_from_granules.py`;
+   do
+       python ${pge_dir}/run_mdn.py $a $output_dir;
+
+  done
+
+tar -czvf $output_dir.tar.gz $output_dir
+rm -r $output_dir
