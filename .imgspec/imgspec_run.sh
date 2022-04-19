f8fb03a5e9b1955da7b6cd9b7ef46473b5d547dd
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-19 08:51:26 -0400

    Typo fix

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,7 +1,6 @@
 imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
-source activate base
 rm ${pge_dir}/MDN/Weights/HICO/*
 wget -P ${pge_dir}/MDN/Weights/HICO https://github.com/EnSpec/sister-mdn_chlorophyll/raw/master/Weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip
 
@@ -23,7 +22,7 @@ fi
 #echo $out_dir
 mkdir output/$out_dir
 
-#tar -xzvf $tar_file -C input
+tar -xzvf $tar_file -C input
 
 for a in `python ${imgspec_dir}/get_paths_from_granules.py`;
    do
