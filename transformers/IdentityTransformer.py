f05c2cd29e343e2825a4ee6578732628f7fdcfa0
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-19 08:41:09 -0400

    Delete

--- transformers/IdentityTransformer.py
+++
@@ -1,8 +0,0 @@
-from ._CustomTransformer import _CustomTransformer
-
-
-class IdentityTransformer(_CustomTransformer):
-	''' No transformation '''
-	def _transform(self, X, *args, **kwargs):         return X
-	def _inverse_transform(self, X, *args, **kwargs): return X
-
