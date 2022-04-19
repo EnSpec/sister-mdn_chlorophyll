6ffd49d19a9a1a2df07c59d03c3187580a1a29e3
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-04-19 08:22:57 -0400

    Updates to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
-from pathlib import Path 
+from setuptools import setup,find_packages
+from pathlib import Path
 from __version__ import __version__
 
 # We can't reference the local requirements file within itself,
 # which means installing with e.g. pip install -r MDN/requirements.txt
-# won't work if we have requirements listed here, and only a '.' in 
-# the requirements file. 
+# won't work if we have requirements listed here, and only a '.' in
+# the requirements file.
 # Instead, we can reference the local path here, and parse the requirements
 with Path(__file__).parent.joinpath('requirements.txt').open() as f:
-	requirements = [line.strip() for line in f.readlines()]
+        requirements = [line.strip() for line in f.readlines()]
 
 setup(
     name='MDN',
@@ -17,8 +17,7 @@ setup(
     author='Brandon Smith',
     author_email='b.smith@nasa.gov',
     url='https://github.com/BrandonSmithJ/MDN',
-    package_dir={'MDN': ''},
-    packages=['MDN'],
+    packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
 )
