1c8299f085ce6b1daf825270fb723ae346e543d6
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-05-02 15:54:40 -0400

    Update weights url link

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -8,5 +8,4 @@ cd $pge_dir
 python setup.py install
 
 rm ${pge_dir}/MDN/Weights/HICO/*
-wget -P ${pge_dir}/MDN/Weights/HICO https://github.com/EnSpec/sister-mdn_chlorophyll/raw/master/Weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip
-
+wget -P ${pge_dir}/MDN/Weights/HICO https://github.com/EnSpec/sister-mdn_chlorophyll/raw/master/MDN/Weights/HICO/45313342cb628c8cf45b6e2e29f4dc9a780ee1d403bdb98461e28fcb13ad9ce3.zip
