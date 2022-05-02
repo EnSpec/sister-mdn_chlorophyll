f475a75871f2e9ac331a4e1874b688a5e281f5a2
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-05-02 16:56:36 -0400

    Added env activation

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -3,6 +3,7 @@ imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
 conda create -y --name sister python=3.8
+source activate sister
 
 cd $pge_dir
 python setup.py install
